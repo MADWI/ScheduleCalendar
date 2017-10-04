85d39332c0b37ae4dcfd7fa1ed0a3056b02b3d88
Marek Macko <macko.marek93@gmail.com>
2017-10-04 16:38:55 +0200

    SchedulePagerAdapter moved to module

---
+++ src/main/java/pl/edu/zut/mad/schedulecalendar/SchedulePagerAdapter.java
@@ -0,0 +1,31 @@
+package pl.edu.zut.mad.schedulecalendar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+
+import com.tobishiba.circularviewpager.library.BaseCircularViewPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class SchedulePagerAdapter extends BaseCircularViewPagerAdapter<Date> {
+
+    public SchedulePagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager, new ArrayList<Date>());
+    }
+
+    @Override
+    protected Fragment getFragmentForItem(Date date) {
+        return ScheduleDayFragment.newInstance(date);
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
+
+    public void setWeekDates(List<Date> weekDates ) {
+        setItems(weekDates);
+    }
+}
