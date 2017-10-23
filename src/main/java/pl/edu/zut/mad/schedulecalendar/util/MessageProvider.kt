fc04e9880c0a228a436e4050cabb77aa6610467a
Marek Macko <macko.marek93@gmail.com>
2017-10-23 17:55:26 +0200

    Handling album not found from api

--- src/main/java/pl/edu/zut/mad/schedulecalendar/util/MessageProvider.kt
+++ src/main/java/pl/edu/zut/mad/schedulecalendar/util/MessageProvider.kt
@@ -9,6 +9,7 @@ class MessageProvider {
     fun getIdByHttpError(error: Throwable): Int {
         if (error is HttpException) {
             when (error.code()) {
+                404 -> return R.string.error_album_number_not_found
                 500 -> return R.string.error_service_internal
                 504 -> return R.string.error_service_database_update
             }
