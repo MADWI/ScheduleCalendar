115f85aac1094e24d80f66eea5e4935dead475e6
Marek Macko <macko.marek93@gmail.com>
2017-11-06 17:14:51 +0100

    DatesProvider tests

--- src/main/java/pl/edu/zut/mad/schedule/util/DatesProvider.kt
+++ src/main/java/pl/edu/zut/mad/schedule/util/DatesProvider.kt
@@ -7,7 +7,7 @@ internal class DatesProvider {
     fun getByInterval(start: LocalDate, end: LocalDate): MutableList<LocalDate> {
         var nextDay = start
         val dates: MutableList<LocalDate> = ArrayList()
-        while (!nextDay.isEqual(end)) {
+        while (!nextDay.isAfter(end)) {
             dates.add(nextDay)
             nextDay = nextDay.plusDays(1)
         }
--- src/test/java/pl/edu/zut/mad/schedule/util/DatesProviderTest.kt
+++ src/test/java/pl/edu/zut/mad/schedule/util/DatesProviderTest.kt
@@ -1,15 +1,36 @@
 package pl.edu.zut.mad.schedule.util
 
+import org.joda.time.Days
 import org.joda.time.LocalDate
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class DatesProviderTest {
 
+    private val startDate = LocalDate.parse("2016-01-01")
+    private val endDate = LocalDate.parse("2017-01-01")
+    private val datesProvider = DatesProvider()
+
     @Test
-    fun getByInterval() {
-        val datesProvider = DatesProvider()
-        val startDate = LocalDate.parse("2016-01-01")
-        val endDate = LocalDate.parse("2017-01-01")
+    fun datesAmountIsProper() {
         val dates = datesProvider.getByInterval(startDate, endDate)
+
+        val daysBetweenInclusive = Days.daysBetween(dates.first(), dates.last()).days + 1
+
+        assertEquals(daysBetweenInclusive, dates.size)
+    }
+
+    @Test
+    fun firstDayIsEqualsStartDate() {
+        val dates = datesProvider.getByInterval(startDate, endDate)
+
+        assertEquals(dates.first(), startDate)
+    }
+
+    @Test
+    fun lastDayIsEqualsStartDate() {
+        val dates = datesProvider.getByInterval(startDate, endDate)
+
+        assertEquals(dates.last(), endDate)
     }
 }
