313640bf27af1ec506a6ee7778341751cfd007a4
Marek Macko <macko.marek93@gmail.com>
2017-11-06 14:40:49 +0100

    Use HttpURLConnection codes

--- src/main/java/pl/edu/zut/mad/schedule/util/MessageProvider.kt
+++ src/main/java/pl/edu/zut/mad/schedule/util/MessageProvider.kt
@@ -2,15 +2,16 @@ package pl.edu.zut.mad.schedule.util
 
 import pl.edu.zut.mad.schedule.R
 import retrofit2.HttpException
+import java.net.HttpURLConnection
 
 internal class MessageProvider {
 
     fun getIdByHttpError(error: Throwable): Int {
         if (error is HttpException) {
             when (error.code()) {
-                404 -> return R.string.error_album_number_not_found
-                503 -> return R.string.error_service_database_update
-                500 -> return R.string.error_service_internal
+                HttpURLConnection.HTTP_NOT_FOUND -> return R.string.error_album_number_not_found
+                HttpURLConnection.HTTP_UNAVAILABLE -> return R.string.error_service_database_update
+                HttpURLConnection.HTTP_INTERNAL_ERROR -> return R.string.error_service_internal
             }
         }
         return R.string.error_service_unrecognized
