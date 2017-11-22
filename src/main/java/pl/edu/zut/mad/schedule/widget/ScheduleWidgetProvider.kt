20ae6aaff6e581145a62bfa8f4aa445baa2fb752
Marek Macko <macko.marek93@gmail.com>
2017-11-22 16:09:01 +0100

    Added widget buttons with actions

--- src/main/java/pl/edu/zut/mad/schedule/widget/ScheduleWidgetProvider.kt
+++ src/main/java/pl/edu/zut/mad/schedule/widget/ScheduleWidgetProvider.kt
@@ -1,10 +1,12 @@
 package pl.edu.zut.mad.schedule.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.text.format.DateFormat
+import android.util.Log
 import android.widget.RemoteViews
 import org.joda.time.LocalDate
 import pl.edu.zut.mad.schedule.R
@@ -13,6 +15,8 @@ internal class ScheduleWidgetProvider : AppWidgetProvider() {
 
     companion object {
         private const val HEADER_DATE_FORMAT = "EEEE, dd-MM-yyyy"
+        private const val PREVIOUS_DAY_BUTTON = "pl.edu.zut.mad.schedule.WIDGET_PREVIOUS_DAY_CLICK"
+        private const val NEXT_DAY_BUTTON = "pl.edu.zut.mad.schedule.WIDGET_NEXT_DAY_CLICK"
     }
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
@@ -30,6 +34,22 @@ internal class ScheduleWidgetProvider : AppWidgetProvider() {
         val remoteViews = RemoteViews(context.packageName, R.layout.widget_lessons)
         remoteViews.setTextViewText(R.id.widgetDateView, headerText)
         remoteViews.setRemoteAdapter(R.id.widgetLessonsList, intent)
+        setupButtonsClickPendingIntents(context, remoteViews)
         return remoteViews
+    }
+
+    private fun setupButtonsClickPendingIntents(context: Context, remoteViews: RemoteViews) {
+        val nextDayPendingIntent = getPendingIntentWithAction(context, NEXT_DAY_BUTTON)
+        val previousDayPendingIntent = getPendingIntentWithAction(context, PREVIOUS_DAY_BUTTON)
+        remoteViews.setOnClickPendingIntent(R.id.widgetNextDayButton, nextDayPendingIntent)
+        remoteViews.setOnClickPendingIntent(R.id.widgetPreviousDayButton, previousDayPendingIntent)
+    }
+
+    private fun getPendingIntentWithAction(context: Context, action: String): PendingIntent =
+        PendingIntent.getBroadcast(context, 0, Intent(action), PendingIntent.FLAG_UPDATE_CURRENT)
+
+    override fun onReceive(context: Context?, intent: Intent) {
+        super.onReceive(context, intent)
+        Log.d("onReceive", intent.action)
     }
 }
