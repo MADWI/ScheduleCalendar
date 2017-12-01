6cf8922bc8386c571d2c8816928410b0a5fc155c
Marek Macko <macko.marek93@gmail.com>
2017-12-01 16:57:30 +0100

    Revert models changes

--- src/main/java/pl/edu/zut/mad/schedule/data/model/ui/Lesson.kt
+++ src/main/java/pl/edu/zut/mad/schedule/data/model/ui/Lesson.kt
@@ -1,11 +1,4 @@
 package pl.edu.zut.mad.schedule.data.model.ui
 
-import android.annotation.SuppressLint
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-import org.joda.time.LocalDate
-
-@SuppressLint("ParcelCreator")
-@Parcelize
 internal data class Lesson(val teacherWithRoom: String, val subjectWithType: String,
-    val isCancelled: Boolean, val timeRange: TimeRange, val date: LocalDate) : Parcelable
+                           val isCancelled: Boolean, val timeRange: TimeRange)
--- src/main/java/pl/edu/zut/mad/schedule/data/model/ui/TimeRange.kt
+++ src/main/java/pl/edu/zut/mad/schedule/data/model/ui/TimeRange.kt
@@ -1,9 +1,3 @@
 package pl.edu.zut.mad.schedule.data.model.ui
 
-import android.annotation.SuppressLint
-import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
-
-@SuppressLint("ParcelCreator")
-@Parcelize
-internal class TimeRange(val start: String, val end: String) : Parcelable
+internal class TimeRange(val start: String, val end: String)
