aead765c702556c7094314760527ccf52c12e424
Marek Macko <macko.marek93@gmail.com>
2017-12-01 16:49:33 +0100

    Parcelable for TimeRange and Lesson model

--- src/main/java/pl/edu/zut/mad/schedule/data/model/ui/TimeRange.kt
+++ src/main/java/pl/edu/zut/mad/schedule/data/model/ui/TimeRange.kt
@@ -1,3 +1,9 @@
 package pl.edu.zut.mad.schedule.data.model.ui
 
-internal class TimeRange(val start: String, val end: String)
+import android.annotation.SuppressLint
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@SuppressLint("ParcelCreator")
+@Parcelize
+internal class TimeRange(val start: String, val end: String) : Parcelable
