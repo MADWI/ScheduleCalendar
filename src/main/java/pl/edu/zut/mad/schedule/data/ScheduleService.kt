396267ef8c036f87214472344ed14a22767272bf
Marek Macko <macko.marek93@gmail.com>
2017-12-06 16:32:56 +0100

    Remove old service method

--- src/main/java/pl/edu/zut/mad/schedule/data/ScheduleService.kt
+++ src/main/java/pl/edu/zut/mad/schedule/data/ScheduleService.kt
@@ -4,21 +4,12 @@ import io.reactivex.Observable
 import pl.edu.zut.mad.schedule.data.model.api.Day
 import retrofit2.http.GET
 import retrofit2.http.Path
-import retrofit2.http.Query
 import retrofit2.http.QueryMap
 
 internal interface ScheduleService {
 
     @GET("{albumNumber}")
     fun fetchScheduleByAlbumNumber(@Path("albumNumber") albumNumber: Int): Observable<List<Day>>
-
-    //TODO delete
-    @GET(".")
-    fun fetchScheduleByQueries(@Query("name") teacherName: String, @Query("surname") teacherSurname: String,
-        @Query("facultyAbbreviation") facultyAbbreviation: String, @Query("subject") subject: String,
-        @Query("fieldOfStudy") fieldOfStudy: String, @Query("courseType") courseType: String,
-        @Query("semester") semester: Int?, @Query("form") form: String,
-        @Query("dateFrom") dateFrom: String, @Query("dateTo") dateTo: String): Observable<List<Day>>
 
     @GET(".")
     fun fetchScheduleByQueries(@QueryMap queries: Map<String, String>): Observable<List<Day>>
