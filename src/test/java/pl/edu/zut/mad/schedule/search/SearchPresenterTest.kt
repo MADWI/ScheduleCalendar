d7bf8bd20c5e24086da2ac6f4ee5f9dc855052be
Marek Macko <macko.marek93@gmail.com>
2017-12-06 16:42:31 +0100

    Fix SearchPresenter tests with new service method

--- src/test/java/pl/edu/zut/mad/schedule/search/SearchPresenterTest.kt
+++ src/test/java/pl/edu/zut/mad/schedule/search/SearchPresenterTest.kt
@@ -1,7 +1,6 @@
 package pl.edu.zut.mad.schedule.search
 
 import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.eq
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.verify
 import com.nhaarman.mockito_kotlin.whenever
@@ -62,7 +61,7 @@ internal class SearchPresenterTest {
 
         presenter.onSearch()
 
-        verify(service).fetchScheduleByQueries(any(), any(), any(), any(), any(), any(), eq(null), any(), any(), any())
+        verify(service).fetchScheduleByQueries(any())
     }
 
     @Test
@@ -102,22 +101,7 @@ internal class SearchPresenterTest {
     }
 
     private fun prepareServiceMockToReturnObservable(observable: Observable<List<Day>>) {
-        prepareViewMocksToReturnQuery()
         whenever(networkConnection.isAvailable()).thenReturn(true)
-        whenever(service.fetchScheduleByQueries(any(), any(), any(), any(), any(), any(), eq(null),
-            any(), any(), any())).thenReturn(observable)
-    }
-
-    private fun prepareViewMocksToReturnQuery() {
-        whenever(view.getTeacherName()).thenReturn("")
-        whenever(view.getTeacherSurname()).thenReturn("")
-        whenever(view.getFacultyAbbreviation()).thenReturn("")
-        whenever(view.getSubject()).thenReturn("")
-        whenever(view.getFieldOfStudy()).thenReturn("")
-        whenever(view.getCourseType()).thenReturn("")
-        whenever(view.getSemester()).thenReturn(null)
-        whenever(view.getForm()).thenReturn("")
-        whenever(view.getDateFrom()).thenReturn("")
-        whenever(view.getDateTo()).thenReturn("")
+        whenever(service.fetchScheduleByQueries(any())).thenReturn(observable)
     }
 }
