a3121493126afdd97a71b4a8ee782ed1bf7fb9b2
Marek Macko <macko.marek93@gmail.com>
2017-12-19 14:54:01 +0100

    AnimationSettings

--- src/main/java/pl/edu/zut/mad/schedule/animation/AnimationModule.kt
+++ src/main/java/pl/edu/zut/mad/schedule/animation/AnimationModule.kt
@@ -2,31 +2,27 @@ package pl.edu.zut.mad.schedule.animation
 
 import dagger.Module
 import dagger.Provides
-import pl.edu.zut.mad.schedule.R
 import javax.inject.Named
 
 @Module
 internal class AnimationModule(private val enterAnimationParams: AnimationParams) {
 
     companion object {
-        //TODO clean up
-        const val ENTER_COLOR_ANIMATION_NAME ="enter_color_animation_name"
-        const val EXIT_COLOR_ANIMATION_NAME ="exit_color_animation_name"
-        const val ENTER_REVEAL_ANIMATION_NAME ="enter_reveal_animation_name"
-        const val EXIT_REVEAL_ANIMATION_NAME ="exit_reveal_animation_name"
-        private val ENTER_START_COLOR_ID = R.color.scheduleColorPrimaryDark
-        private val ENTER_END_COLOR_ID = android.R.color.transparent
-        private val EXIT_START_COLOR_ID = android.R.color.transparent
-        private val EXIT_END_COLOR_ID = R.color.scheduleColorPrimaryDark
+        const val ENTER_COLOR_ANIMATION_NAME = "enter_color_animation_name"
+        const val EXIT_COLOR_ANIMATION_NAME = "exit_color_animation_name"
+        const val ENTER_REVEAL_ANIMATION_NAME = "enter_reveal_animation_name"
+        const val EXIT_REVEAL_ANIMATION_NAME = "exit_reveal_animation_name"
     }
 
     @Provides
     @Named(ENTER_COLOR_ANIMATION_NAME)
-    fun provideEnterColorAnimation() = ColorAnimation(ENTER_START_COLOR_ID, ENTER_END_COLOR_ID)
+    fun provideEnterColorAnimation() =
+        ColorAnimation(AnimationSettings.ENTER_START_COLOR_ID, AnimationSettings.ENTER_END_COLOR_ID)
 
     @Provides
     @Named(EXIT_COLOR_ANIMATION_NAME)
-    fun provideExitColorAnimation() = ColorAnimation(EXIT_START_COLOR_ID, EXIT_END_COLOR_ID)
+    fun provideExitColorAnimation() =
+        ColorAnimation(AnimationSettings.EXIT_START_COLOR_ID, AnimationSettings.EXIT_END_COLOR_ID)
 
     @Provides
     @Named(ENTER_REVEAL_ANIMATION_NAME)
