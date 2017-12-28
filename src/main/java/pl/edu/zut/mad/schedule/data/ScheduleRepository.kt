a729bdaee765f0b1fb9801d350d19d7b06227cbd
Marek Macko <macko.marek93@gmail.com>
2017-12-28 13:05:16 +0100

    Closing database after transaction

--- src/main/java/pl/edu/zut/mad/schedule/data/ScheduleRepository.kt
+++ src/main/java/pl/edu/zut/mad/schedule/data/ScheduleRepository.kt
@@ -2,8 +2,6 @@ package pl.edu.zut.mad.schedule.data
 
 import io.reactivex.Completable
 import io.reactivex.Observable
-import io.reactivex.disposables.Disposable
-import io.reactivex.schedulers.Schedulers
 import org.joda.time.LocalDate
 import pl.edu.zut.mad.schedule.data.model.ui.EmptyDay
 import pl.edu.zut.mad.schedule.data.model.ui.OptionalDay
@@ -18,30 +16,44 @@ internal class ScheduleRepository(private val database: ScheduleDatabase, privat
 
     fun save(days: List<DayApi>): Completable =
         Completable.fromCallable {
-            database.instance.executeTransaction({ it.copyToRealm(days) })
+            val instance = database.instance
+            instance.executeTransaction { it.copyToRealm(days) }
+            instance.close()
         }
 
-    fun delete(): Disposable =
-        Observable.fromCallable { database.instance.executeTransaction { it.deleteAll() } }
-            .subscribeOn(Schedulers.io())
-            .subscribe()
+    fun delete() {
+        val instance = database.instance
+        instance.executeTransactionAsync { it.deleteAll() }
+        instance.close()
+    }
 
     fun getDayByDate(date: LocalDate): Observable<OptionalDay> {
-        val day = database.instance.where(DayApi::class.java)
+        val instance = database.instance
+        val day = instance.where(DayApi::class.java)
             .equalTo(DATE_COLUMN, date.toDate())
             .findFirst()
         val optionalDay: OptionalDay =
             if (day == null) EmptyDay(date) else mapper.toDayUiFromApi(day)
+        instance.close()
         return Observable.just(optionalDay)
     }
 
-    fun getScheduleMinDate(): LocalDate = database.instance
-        .where(DayApi::class.java)
-        .minimumDate(DATE_COLUMN)
-        .let { mapper.toUiDate(it) }
+    fun getScheduleMinDate(): LocalDate {
+        val instance = database.instance
+        val date = instance.where(DayApi::class.java)
+            .minimumDate(DATE_COLUMN)
+            .let { mapper.toUiDate(it) }
+        instance.close()
+        return date
+    }
 
-    fun getScheduleMaxDate(): LocalDate = database.instance
-        .where(DayApi::class.java)
-        .maximumDate(DATE_COLUMN)
-        .let { mapper.toUiDate(it) }
+    fun getScheduleMaxDate(): LocalDate {
+        val instance = database.instance
+        val date = instance
+            .where(DayApi::class.java)
+            .maximumDate(DATE_COLUMN)
+            .let { mapper.toUiDate(it) }
+        instance.close()
+        return date
+    }
 }
