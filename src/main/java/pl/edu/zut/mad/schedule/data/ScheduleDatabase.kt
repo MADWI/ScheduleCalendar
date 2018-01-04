7ac2b7fc1213a891ea219eacd43ac3e214d9b62b
Marek Macko <macko.marek93@gmail.com>
2018-01-04 16:29:41 +0100

    Hide realm in ScheduleDatabase

--- src/main/java/pl/edu/zut/mad/schedule/data/ScheduleDatabase.kt
+++ src/main/java/pl/edu/zut/mad/schedule/data/ScheduleDatabase.kt
@@ -1,9 +1,52 @@
 package pl.edu.zut.mad.schedule.data
 
 import io.realm.Realm
+import pl.edu.zut.mad.schedule.data.model.api.Day
+import java.util.Date
 
 internal class ScheduleDatabase {
 
-    val instance: Realm
-        get() = Realm.getDefaultInstance()
+    companion object {
+        const val DATE_COLUMN = "date"
+    }
+
+    // TODO rename database variable
+    fun save(days: List<Day>) {
+        val database = getDatabase()
+        database.executeTransaction { it.copyToRealm(days) }
+        database.close()
+    }
+
+    fun delete() {
+        val database = getDatabase()
+        database.executeTransactionAsync { it.deleteAll() }
+        database.close()
+    }
+
+    fun findDayWithDate(date: Date): Day? {
+        val database = getDatabase()
+        var day: Day? = database.where(Day::class.java)
+            .equalTo(DATE_COLUMN, date)
+            .findFirst() ?: return null
+        day = database.copyFromRealm(day)
+        database.close()
+        return day
+    }
+
+    fun findMinimumDate(): Date? {
+        val database = getDatabase()
+        val date = database.where(Day::class.java).minimumDate(DATE_COLUMN)
+        database.close()
+        return date
+    }
+
+    fun findMaximumDate(): Date? {
+        val database = getDatabase()
+        val date = database.where(Day::class.java).maximumDate(DATE_COLUMN)
+        database.close()
+        return date
+    }
+
+    // TODO inject configuration with migration
+    private fun getDatabase() = Realm.getDefaultInstance()
 }
--- src/main/java/pl/edu/zut/mad/schedule/data/ScheduleRepository.kt
+++ src/main/java/pl/edu/zut/mad/schedule/data/ScheduleRepository.kt
@@ -10,50 +10,27 @@ import pl.edu.zut.mad.schedule.data.model.api.Day as DayApi
 
 internal class ScheduleRepository(private val database: ScheduleDatabase, private val mapper: ModelMapper) {
 
-    companion object {
-        const val DATE_COLUMN = "date"
-    }
-
     fun save(days: List<DayApi>): Completable =
         Completable.fromCallable {
-            val instance = database.instance
-            instance.executeTransaction { it.copyToRealm(days) }
-            instance.close()
+            database.save(days)
         }
 
-    fun delete() {
-        val instance = database.instance
-        instance.executeTransactionAsync { it.deleteAll() }
-        instance.close()
-    }
+    fun delete() = database.delete()
 
     fun getDayByDate(date: LocalDate): Observable<OptionalDay> {
-        val instance = database.instance
-        val day = instance.where(DayApi::class.java)
-            .equalTo(DATE_COLUMN, date.toDate())
-            .findFirst()
+        val day = database.findDayWithDate(date.toDate())
         val optionalDay: OptionalDay =
             if (day == null) EmptyDay(date) else mapper.toDayUiFromApi(day)
-        instance.close()
         return Observable.just(optionalDay)
     }
 
     fun getScheduleMinDate(): LocalDate {
-        val instance = database.instance
-        val date = instance.where(DayApi::class.java)
-            .minimumDate(DATE_COLUMN)
-            .let { mapper.toUiDate(it) }
-        instance.close()
-        return date
+        val date = database.findMinimumDate()
+        return mapper.toUiDate(date)
     }
 
     fun getScheduleMaxDate(): LocalDate {
-        val instance = database.instance
-        val date = instance
-            .where(DayApi::class.java)
-            .maximumDate(DATE_COLUMN)
-            .let { mapper.toUiDate(it) }
-        instance.close()
-        return date
+        val date = database.findMaximumDate()
+        return mapper.toUiDate(date)
     }
 }
