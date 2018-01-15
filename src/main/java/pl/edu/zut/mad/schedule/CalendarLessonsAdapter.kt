f3d985999b793d30e4745fb379b23cedb2e1972e
Marek Macko <macko.marek93@gmail.com>
2018-01-15 18:35:31 +0100

    Change cancelled lesson view

--- src/main/java/pl/edu/zut/mad/schedule/CalendarLessonsAdapter.kt
+++ src/main/java/pl/edu/zut/mad/schedule/CalendarLessonsAdapter.kt
@@ -1,7 +1,12 @@
 package pl.edu.zut.mad.schedule
 
 import android.content.Context
+import android.support.v4.content.ContextCompat
+import android.text.Spannable
+import android.text.Spanned
+import android.text.style.StrikethroughSpan
 import android.view.View
+import android.widget.TextView
 import com.ognev.kotlin.agendacalendarview.models.CalendarEvent
 import com.ognev.kotlin.agendacalendarview.render.DefaultEventAdapter
 import kotlinx.android.synthetic.main.lesson_calendar_item.view.*
@@ -14,6 +19,10 @@ import pl.edu.zut.mad.schedule.util.LessonItemPainter
 import java.util.Calendar
 
 internal class CalendarLessonsAdapter(context: Context) : DefaultEventAdapter() {
+
+    companion object {
+        private val STRIKE_THROUGH_SPAN = StrikethroughSpan()
+    }
 
     private val lessonItemPainter = LessonItemPainter(context)
 
@@ -48,9 +57,23 @@ internal class CalendarLessonsAdapter(context: Context) : DefaultEventAdapter() 
             view.subjectWithTypeView.text = lessonFormatter.getSubjectWithType()
             view.teacherWithRoomView.text = lessonFormatter.getTeacherWithRoom()
             if (isCancelled) {
-                view.cancelledStatusTextView.visibility = View.VISIBLE
+                view.lessonCalendarItemLayout.foreground =
+                    ContextCompat.getDrawable(view.context, R.drawable.red_border)
+                modifyViewsToCancelled(view)
             } else if (isExam) {
                 view.examStatusTextView.visibility = View.VISIBLE
             }
         }
+
+    private fun modifyViewsToCancelled(view: View) {
+        modifyToCancelled(view.subjectWithTypeView)
+        modifyToCancelled(view.teacherWithRoomView)
+    }
+
+    private fun modifyToCancelled(textView: TextView) {
+        val text = textView.text
+        textView.setText(text, TextView.BufferType.SPANNABLE)
+        val spannable = textView.text as Spannable
+        spannable.setSpan(STRIKE_THROUGH_SPAN, 0, text.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+    }
 }
