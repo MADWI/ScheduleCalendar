d7ac2f04bf1c713e77327fdf737c72b88a6d02e5
Marek Macko <marek.macko@intive.com>
2019-03-30 12:23:51 +0100

    Display  ScheduleFragment

---
+++ app/src/main/java/pl/zut/mad/scheduleapp/App.kt
@@ -0,0 +1,6 @@
+package pl.zut.mad.scheduleapp
+
+import pl.edu.zut.mad.schedule.app.ScheduleApp
+
+
+class App : ScheduleApp()
--- app/src/main/java/pl/zut/mad/scheduleapp/MainActivity.kt
+++ app/src/main/java/pl/zut/mad/scheduleapp/MainActivity.kt
@@ -2,11 +2,15 @@ package pl.zut.mad.scheduleapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import pl.edu.zut.mad.schedule.ScheduleFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.mainContainerView, ScheduleFragment())
+            .commit()
     }
 }
