b62bf13b56a6f97e10b6e2afee37ea79a57743e1
Marek Macko <marek.macko@intive.com>
2019-03-30 17:00:24 +0100

    Added CalendarActivity for testing purposes

---
+++ schedule/src/main/java/pl/edu/zut/mad/schedule/calendar/CalendarActivity.kt
@@ -0,0 +1,13 @@
+package pl.edu.zut.mad.schedule.calendar
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import pl.edu.zut.mad.schedule.R
+
+class CalendarActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calendar)
+    }
+}
