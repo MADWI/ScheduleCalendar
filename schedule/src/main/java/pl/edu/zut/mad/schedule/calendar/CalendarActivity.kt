c7700c067021242e17ae7397aee17c84535d8198
Marek Macko <macko.marek93@gmail.com>
2019-05-01 15:26:03 +0200

    Calendar pager (#45)

--- schedule/src/main/java/pl/edu/zut/mad/schedule/calendar/CalendarActivity.kt
+++ schedule/src/main/java/pl/edu/zut/mad/schedule/calendar/CalendarActivity.kt
@@ -2,6 +2,7 @@ package pl.edu.zut.mad.schedule.calendar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_calendar.*
 import pl.edu.zut.mad.schedule.R
 
 class CalendarActivity : AppCompatActivity() {
@@ -9,5 +10,6 @@ class CalendarActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calendar)
+        calendarViewPager.adapter = CalendarFragmentAdapter(this)
     }
 }
