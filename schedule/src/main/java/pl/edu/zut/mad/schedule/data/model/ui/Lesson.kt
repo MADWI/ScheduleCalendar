47db393a08acf46f091ce207b4ce66f7bb0a438a
Marek Macko <marek.macko@intive.com>
2019-05-02 15:55:55 +0200

    Remove @SuppressLint("ParcelCreator")

--- schedule/src/main/java/pl/edu/zut/mad/schedule/data/model/ui/Lesson.kt
+++ schedule/src/main/java/pl/edu/zut/mad/schedule/data/model/ui/Lesson.kt
@@ -1,11 +1,9 @@
 package pl.edu.zut.mad.schedule.data.model.ui
 
-import android.annotation.SuppressLint
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import org.joda.time.LocalDate
 
-@SuppressLint("ParcelCreator")
 @Parcelize
 internal data class Lesson(val subject: String, val type: String,
     val room: String, val teacher: Teacher,
--- schedule/src/main/java/pl/edu/zut/mad/schedule/data/model/ui/TimeRange.kt
+++ schedule/src/main/java/pl/edu/zut/mad/schedule/data/model/ui/TimeRange.kt
@@ -1,9 +1,7 @@
 package pl.edu.zut.mad.schedule.data.model.ui
 
-import android.annotation.SuppressLint
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
-@SuppressLint("ParcelCreator")
 @Parcelize
 internal class TimeRange(val start: String, val end: String) : Parcelable
