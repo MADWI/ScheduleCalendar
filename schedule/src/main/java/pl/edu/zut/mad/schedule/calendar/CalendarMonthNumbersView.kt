442de8f4b3740c59063728b5979fa8c108e2b269
Marek Macko <macko.marek93@gmail.com>
2019-05-27 20:38:42 +0200

    Selected day with indicator (#52)
    * Selected day with indicator
    
    * Remove setup calendar day view from pager adapter

--- schedule/src/main/java/pl/edu/zut/mad/schedule/calendar/CalendarMonthNumbersView.kt
+++ schedule/src/main/java/pl/edu/zut/mad/schedule/calendar/CalendarMonthNumbersView.kt
@@ -11,19 +11,65 @@ import org.joda.time.LocalDate
 import pl.edu.zut.mad.schedule.R
 import pl.edu.zut.mad.schedule.util.forEachChild
 
-internal class CalendarMonthNumbersView(context: Context, attributeSet: AttributeSet) : LinearLayout(context, attributeSet) {
+internal class CalendarMonthNumbersView(context: Context, attributeSet: AttributeSet) :
+    LinearLayout(context, attributeSet) {
+
+    private var selectedDayView: TextView? = null
+        set(value) {
+            field?.setBackgroundResource(0)
+            value?.setBackgroundResource(R.drawable.calendar_day_selected_background)
+            field = value
+        }
 
     init {
         View.inflate(context, R.layout.calendar_month_numbers, this)
         orientation = VERTICAL
     }
 
-    fun setup(monthFirstDate: LocalDate, onDay: (TextView, LocalDate) -> Unit) {
+    fun setup(lessonDays: List<LocalDate>, dateClickListener: (LocalDate) -> Unit) {
+        val monthFirstDate = lessonDays[0].withDayOfMonth(1)
         var date = monthFirstDate.withDayOfWeek(DateTimeConstants.MONDAY)
+        forEachDayView {
+            setupDayView(it, date, lessonDays, dateClickListener)
+            date = date.plusDays(1)
+        }
+    }
+
+    fun setSelectedDate(date: LocalDate) {
+        selectedDayView = findDayViewByDate(date)
+    }
+
+    private fun setupDayView(
+        dayView: TextView,
+        date: LocalDate,
+        lessonDays: List<LocalDate>,
+        dateClickListener: (LocalDate) -> Unit
+    ) {
+        dayView.tag = date
+        dayView.text = date.dayOfMonth.toString()
+        dayView.setOnClickListener {
+            selectedDayView = dayView
+            dateClickListener(date)
+        }
+        if (date in lessonDays) {
+            dayView.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, R.drawable.calendar_day_lessons_indicator)
+        }
+    }
+
+    private fun findDayViewByDate(date: LocalDate): TextView? {
+        var dayView: TextView? = null
+        forEachDayView {
+            if (it.tag as LocalDate == date) {
+                dayView = it
+            }
+        }
+        return dayView
+    }
+
+    private fun forEachDayView(onDay: (TextView) -> Unit) {
         forEachChild<ViewGroup> { weekView, _ ->
             weekView.forEachChild<TextView> { dayView, _ ->
-                onDay(dayView, date)
-                date = date.plusDays(1)
+                onDay(dayView)
             }
         }
     }
--- schedule/src/main/java/pl/edu/zut/mad/schedule/calendar/CalendarPagerAdapter.kt
+++ schedule/src/main/java/pl/edu/zut/mad/schedule/calendar/CalendarPagerAdapter.kt
@@ -1,7 +1,6 @@
 package pl.edu.zut.mad.schedule.calendar
 
 import android.view.View
-import android.widget.TextView
 import kotlinx.android.synthetic.main.calendar_month.view.*
 import org.joda.time.LocalDate
 import pl.edu.zut.mad.schedule.R
@@ -9,26 +8,9 @@ import pl.edu.zut.mad.schedule.R
 internal class CalendarPagerAdapter(monthDays: List<List<LocalDate>>, private val onDateClick: (LocalDate) -> Unit) :
     PagerAdapter<List<LocalDate>>(monthDays, R.layout.calendar_month) {
 
-    private var selectedDateView: View? = null
-
     override fun onBind(itemView: View, dates: List<LocalDate>) {
         val monthFirstDay = dates[0].withDayOfMonth(1)
         itemView.monthNameView.text = monthFirstDay.toString(Format.MONTH_NAME)
-        itemView.calendarMonthNumbersView.setup(monthFirstDay) { dayView, date ->
-            setupDayView(dayView, date, dates)
-        }
-    }
-
-    private fun setupDayView(dayView: TextView, date: LocalDate, lessonDays: List<LocalDate>) {
-        dayView.text = date.dayOfMonth.toString()
-        dayView.setOnClickListener {
-            selectedDateView?.setBackgroundResource(0)
-            selectedDateView = it
-            dayView.setBackgroundResource(R.drawable.calendar_day_selected_background)
-            onDateClick(date)
-        }
-        if (date in lessonDays) {
-            dayView.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, R.drawable.calendar_day_lessons_indicator)
-        }
+        itemView.calendarMonthNumbersView.setup(dates, onDateClick)
     }
 }
